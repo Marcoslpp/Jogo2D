e501a1ccf88be8877826531eb10bd7a17e5a19e9
Marcoslpp <132162191+Marcoslpp@users.noreply.github.com>
2024-02-22 20:29:47 -0300

    Algumas mudancas

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,12 +4,43 @@ var velocity = Vector2.ZERO
 var move_speed = 480
 var gravity = 1200
 var jump_force = -720
-
+var is_grounded
+onready var raycasts = $ray_casts
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
+	
+	_get_input()
+	
+	velocity = move_and_slide(velocity)
+	
+	is_grounded = _check_is_grounded()
+	
+	_set_animation()
+	
+func _get_input():
+	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	if(Input.is_action_pressed("jump")):
-		velocity.y = jump_force/2 
-	velocity.x = move_speed * move_direction
-	move_and_slide(velocity)
+	velocity.x = lerp(velocity.x, move_speed * move_direction,0.2)
+	
+	if move_direction != 0:
+		$Texture.scale.x = move_direction
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and is_grounded:
+		velocity.y = jump_force/1.8
+		
+func _check_is_grounded():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+	return false		
+
+func _set_animation():
+	var anim = "idle"
+	if !is_grounded:
+		anim = "jump"
+	elif velocity.x != 0:
+		anim = "run"
+	if $Anim.assigned_animation != anim:
+		$Anim.play(anim)
+	
